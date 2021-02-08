799014db5d22c1b9fe41d99542ad2e760fae91e1
huhuhan <869108683@qq.com>
2021-02-08 17:02:17 +0800

    BPM系统：驳回组件，支持回收并行任务

--- src/views/bpm/components/bpm/task-reject.vue
+++ src/views/bpm/components/bpm/task-reject.vue
@@ -1,31 +1,35 @@
 <template>
-    <el-dialog
-            :v-loading="loading"
-            :append-to-body="isInnerDialog"
-            key="taskHistoryDialog"
-            :visible.sync="visible"
-            :before-close="close"
-            @open="openInit">
-        <div slot="title" class="dialog-title">
-            <span>驳回</span>
-        </div>
+  <el-dialog
+          :v-loading="loading"
+          :append-to-body="isInnerDialog"
+          key="taskHistoryDialog"
+          :visible.sync="visible"
+          :before-close="close"
+          @open="openInit">
+    <div slot="title" class="dialog-title">
+      <span>驳回</span>
+    </div>
 
-        <el-form ref="form" :model="form" label-width="80px">
-            <el-form-item label="备注">
-                <el-input type="textarea" v-model="form.opinion"></el-input>
-            </el-form-item>
-        </el-form>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="备注">
+        <el-input type="textarea" v-model="form.opinion"></el-input>
+      </el-form-item>
+      <el-form-item label="回收" v-show="showRecycleBtn">
+        <span>并行的其他待办任务</span>
+        <el-switch v-model="form.delivery"></el-switch>
+      </el-form-item>
+    </el-form>
 
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="close">取 消</el-button>
-            <el-button type="primary" @click="handleConfirm">确 定</el-button>
-        </div>
-    </el-dialog>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="close">取 消</el-button>
+      <el-button type="primary" @click="handleConfirm">确 定</el-button>
+    </div>
+  </el-dialog>
 </template>
 
 <script>
-  import { BpmTaskDoAction } from '@/api/bpm/wf'
-  import { BpmTaskAction } from '@/api/bpm/constant'
+  import {BpmTaskDoAction} from '@/api/bpm/wf'
+  import {BpmTaskAction} from '@/api/bpm/constant'
 
   export default {
     name: 'task-reject',
@@ -45,39 +49,52 @@
         default: false
       }
     },
-    data () {
+    data() {
       return {
         loading: false,
         form: {
-          opinion: ''
-        }
+          opinion: '',
+          delivery: false
+        },
+        showRecycleBtn: false
       }
     },
     watch: {
-      task (newVal, oldVal) {
+      task(newVal, oldVal) {
         this.$log.primary(`流程驳回窗口-任务对象发生变化，new-id： ${newVal ? newVal.id : null}`)
       }
     },
     methods: {
-      close () {
+      close() {
         // el-dialog的关闭前调用，参考https://www.cnblogs.com/yeqrblog/p/9141701.html
         //直接调用更改.sync
         this.$emit('update:visible', false)
       },
-      openInit () {
+      openInit() {
         this.form.opinion = ''
+        this.form.delivery = false
+        this.showRecycleBtn = false
         this.$log.default('初始化驳回弹窗')
       },
-      handleConfirm () {
-        this.loading = true
-        BpmTaskDoAction({
+      handleConfirm() {
+        let param = {
+          // destination: 'UserTask2',
+          // destination: 'ParallelGateway2',
+          extendConf: {
+            confirmRecycle: this.form.delivery
+          },
           action: BpmTaskAction.reject.key,
           defId: this.task.defId,
           instanceId: this.task.instId,
           nodeId: this.task.nodeId,
           opinion: this.form.opinion,
           taskId: this.task.id
-        }).then(res => {
+        }
+        this.loading = true
+        this.confirmToSubmit(param)
+      },
+      confirmToSubmit(param) {
+        BpmTaskDoAction(param).then(res => {
           if (res.isOk) {
             this.$message.success(res.msg)
             this.close()
@@ -87,11 +104,16 @@
           } else {
             this.$message.error(res.msg)
           }
+          this.loading = true
+        }).catch((error) => {
+          if (error.message.indexOf('需要回收') !== -1) {
+            this.showRecycleBtn = true
+          }
         }).finally(() => {
           this.loading = true
         })
       },
-      toCallBack(){
+      toCallBack() {
         this.$emit('callBack')
       }
     }
